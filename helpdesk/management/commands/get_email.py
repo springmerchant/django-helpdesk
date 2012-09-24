c29fc4e8e99b720fef2c948de17f8177486ba330
Emilian Felecan <emil@springmerchant>
2012-09-24 16:20:55 +0300

    Remove reply-to precedence.

--- helpdesk/management/commands/get_email.py
+++ helpdesk/management/commands/get_email.py
@@ -152,13 +152,7 @@ def ticket_from_message(message, queue, quiet):
     subject = decode_mail_headers(decodeUnknown(message.get_charset(), subject))
     subject = subject.replace("Re: ", "").replace("Fw: ", "").replace("RE: ", "").replace("FW: ", "").strip()
 
-    reply_to = message.get('reply-to', None)
-    
-    if reply_to:
-        sender = reply_to
-    else:
-        sender = message.get('from', _('Unknown Sender'))
-    
+    sender = message.get('from', _('Unknown Sender'))
     sender = decode_mail_headers(decodeUnknown(message.get_charset(), sender))
 
     sender_email = parseaddr(sender)[1]
